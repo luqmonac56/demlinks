2d7d55a64ef7a7dc9856b5baa5808627284984a8
luqmonac56 <luqmonac56@gmail.com>
2021-03-20 23:09:43 +0100

    second commit

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+
+    <div class="text-red-900">
+        Staff Engineer
+      </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
